5c0b3c5960c0c262ce8959be4610e550ebff969f
330059579 <wyunlong@yeah.net>
2020-01-05 22:43:21 +0800

    commit

---
+++ tuanzhang-ad-service/ad-search/src/main/java/com/tuanzhang/ad/mysql/dto/Template.java
@@ -0,0 +1,17 @@
+package com.tuanzhang.ad.mysql.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Template {
+
+    private String databases;
+
+    private List<JsonTable> tableList;
+}
