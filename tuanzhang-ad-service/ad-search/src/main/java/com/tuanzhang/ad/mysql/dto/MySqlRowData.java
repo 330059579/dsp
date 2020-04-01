8dc1c9029ae1afe8fd0271a34d8f60514e0e812d
330059579 <wyunlong@yeah.net>
2020-04-01 08:38:21 +0800

    commit

--- tuanzhang-ad-service/ad-search/src/main/java/com/tuanzhang/ad/mysql/dto/MySqlRowData.java
+++ tuanzhang-ad-service/ad-search/src/main/java/com/tuanzhang/ad/mysql/dto/MySqlRowData.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -20,5 +21,5 @@ public class MySqlRowData {
 
     private OpType opType;
 
-    private List<Map<String, String>> filedValueMap;
+    private List<Map<String, String>> filedValueMap = new ArrayList<>();
 }
