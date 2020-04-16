523cff5d1854dc97f2577525af917b37b23a2a51
330059579 <wyunlong@yeah.net>
2020-04-16 22:11:47 +0800

    commit

--- tuanzhang-ad-service/ad-search/src/main/java/com/tuanzhang/ad/mysql/dto/TableTemplate.java
+++ tuanzhang-ad-service/ad-search/src/main/java/com/tuanzhang/ad/mysql/dto/TableTemplate.java
@@ -17,5 +17,7 @@ public class TableTemplate {
       private String tableName;
       private Integer level;
       private Map<OpType, List<String>> opTypeFieldSetMap = new HashMap<>();
+
+      //由于binlong日志中，只记录列的索引，没有记录列的名字，这里需要维护一个列索引和列名字的映射
       private Map<Integer, String> postMap = new HashMap<>();
 }
