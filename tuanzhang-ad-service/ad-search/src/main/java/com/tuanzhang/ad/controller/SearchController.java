5b688c67a30364091a71d86fee2d923d6fb6f20a
330059579 <wyunlong@yeah.net>
2020-04-17 08:33:49 +0800

    commit

--- tuanzhang-ad-service/ad-search/src/main/java/com/tuanzhang/ad/controller/SearchController.java
+++ tuanzhang-ad-service/ad-search/src/main/java/com/tuanzhang/ad/controller/SearchController.java
@@ -18,6 +18,87 @@ import org.springframework.web.client.RestTemplate;
 import javax.annotation.Resource;
 import java.util.List;
 
+//Ribbon 用来实现负载均衡和远程调用
+/*
+   启动类注册RestTemplate
+   @Bean
+    @LoadBalanced //如果系统有多个实例，该注解可以实现负载均衡,如果投放系统有多个，可以轮询调用
+    RestTemplate restTemplate(){
+        return new RestTemplate();}
+       -------------------------------------------
+    Controller中注入   private final RestTemplate restTemplate;
+    --------------------------------------------------------------
+    调用服务
+    restTemplate.postForEntity(
+                "http://eureka-client-ad-sponsor/ad-sponsor/get/plan",
+                request,
+                BaseResult.class
+        ).getBody();
+
+*/
+
+//feign 调用  ，现实中一般用feign
+/*
+
+启动类添加注解
+@EnableFeignClients //使用feign访问其他微服务
+@EnableEurekaClient //作为一个EurekaClient 存在
+@EnableHystrix //使用断路器
+@EnableCircuitBreaker //断路器
+@EnableDiscoveryClient //微服务的发现
+@EnableHystrixDashboard //微服务监控
+
+---------------------------------------------------------
+  feign:
+  hystrix:
+    enabled: true
+
+management:
+  endpoints:
+    web:
+      exposure:
+         include: "*" #暴露全部监控信息
+
+ -------------------------------------------------------------
+
+ 创建一个查询接口
+ eureka-client-ad-sponsor是要调用服务的名称
+@FeignClient(value = "eureka-client-ad-sponsor", fallback = SponsorClientHystrix.class) //服务降级，指定断路器
+public interface SponsorClient {
+
+    //定义一个接口就可以使用feign调用
+    @RequestMapping(value = "/ad-sponsor/get/plan", method = RequestMethod.POST)
+    BaseResult<List<AdPlan>> getAdPlans(@RequestBody AdPlanGetRquest request);
+}
+
+-----------------------------------------------------------
+Controller中调用查询接口
+
+   注入接口
+   @Resource
+    private SponsorClient sponsorClient;
+
+@IgnoreResponseAdvice
+    @PostMapping("/getAdPlans")
+    public BaseResult<List<AdPlan>> getAdPlanByFeign(@RequestBody AdPlanGetRquest request){
+        return sponsorClient.getAdPlans(request);
+    }
+
+ ---------------------------------------------------------------------
+ 指定短路器
+
+ 定义一个断路器类实现SponsorClient，实现对应方法
+ @Component
+public class SponsorClientHystrix implements SponsorClient {
+
+    @Override
+    public BaseResult<List<AdPlan>> getAdPlans(AdPlanGetRquest request) {
+        return new BaseResult<List<AdPlan>>(1, "断路器执行");
+    }
+}
+
+* */
+
 @Slf4j
 @RestController
 public class SearchController {
